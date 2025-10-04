663c9d33eed6cd0536665fbcf611c87087225599
phu.ma <phu.ma>
2025-10-04 11:06:32 +0700

    update

--- src/domain/warehouse/entities/rack.entity.ts
+++ src/domain/warehouse/entities/rack.entity.ts
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 export class DomainRackEntity extends DomainBaseEntity {
   private name!: string; // Tên của Rack
   private warehouseId!: string; // Gắn với Warehouse
+  private productIds: string[];
 
   constructor(params: {
     id?: string;
@@ -11,6 +12,7 @@ export class DomainRackEntity extends DomainBaseEntity {
     updatedBy?: string;
     name: string;
     warehouseId: string;
+    productIds: string[];
     createdAt?: Date;
     updatedAt?: Date;
   }) {
@@ -24,9 +26,14 @@ export class DomainRackEntity extends DomainBaseEntity {
 
     this.name = params.name;
     this.warehouseId = params.warehouseId;
+    this.productIds = params.productIds;
   }
   setName(name: string): void {
     this.name = name;
+  }
+
+  setProductIds(productIds: string[]): void {
+    this.productIds = productIds;
   }
 
   getName(): string {
@@ -35,5 +42,9 @@ export class DomainRackEntity extends DomainBaseEntity {
 
   getWarehouse(): string {
     return this.warehouseId;
+  }
+
+  getProductIds(): string[] {
+    return this.productIds;
   }
 }
